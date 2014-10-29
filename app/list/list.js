bbb8ee4a285a891e13280872183de4bc2242af08
lsimone <lorenzo.simone@amadeus.com>
2014-10-29 18:46:42 +0100

    added repeat

--- app/list/list.js
+++ app/list/list.js
@@ -1,14 +1,19 @@
 'use strict';
 
-angular.module('myApp.list', ['ngRoute'])
+(function () {
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/list', {
-    templateUrl: 'list/list.html',
-    controller: 'ListCtrl'
-  });
-}])
+    angular.module('myApp.list', ['ngRoute'])
 
-.controller('ListCtrl', [function() {
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/list', {
+                    templateUrl : 'list/list.html',
+                    controller : 'ListCtrl'
+                });
+        }])
 
-}]);
+        .controller('ListCtrl', [function () {
+        	this.todos = ["test", "due", "trete", "sine moi aqquai"];
+
+        }]);
+
+})();
