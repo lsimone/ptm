325349ce63ef6aa36e93fd0caf0156f6ae3d8279
lsimone <lorenzo.simone@amadeus.com>
2014-11-05 20:41:35 +0100

    lista editabile

--- app/list/list.js
+++ app/list/list.js
@@ -2,17 +2,47 @@
 
 (function () {
 
-    angular.module('myApp.list', ['ngRoute'])
+    var INIT_NEW_ENTRY = "enter new entry...";
 
+    var todos = [{
+        text : "test"
+        }, {
+        text : "due"
+        }, {
+        text : "trete"
+        }, {
+        text : "sine moi aqquai"
+    }];
+
+    angular.module('myApp.list', ['ngRoute'])
         .config(['$routeProvider', function ($routeProvider) {
                 $routeProvider.when('/list', {
                     templateUrl : 'list/list.html',
                     controller : 'ListCtrl'
                 });
         }])
-
-        .controller('ListCtrl', [function () {
-        	this.todos = ["test", "due", "trete", "sine moi aqquai"];
+        .controller('ListCtrl', ["$scope", function ($scope) {
+                $scope.todos = todos;
+                $scope.newEntry = "";
+                
+                $scope.addNew = function () {
+                    if ($scope.newEntry && $scope.newEntry != "") {
+                        $scope.todos.unshift({
+                            text : $scope.newEntry
+                        });
+                        $scope.newEntry = "";
+                    }
+                };
+                $scope.setEdit = function (e, index, edit) {
+                    $scope.todos[index].edit = edit;
+                    if (edit) {
+                        var input = document.getElementById("input-" + index);
+                        setTimeout(function () {
+                            input.focus();
+                            console.log(edit, $scope.todos);
+                        }, 1)
+                    }
+                };
 
         }]);
 
