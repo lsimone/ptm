891d7b916fa421e12d67feac702d0b4d9b0e7351
lsimone <lorenzo.simone@amadeus.com>
2014-11-10 06:05:52 +0100

    subtasks

--- app/list/list.js
+++ app/list/list.js
@@ -1,14 +1,17 @@
 'use strict';
 
 (function () {
-        	
+
     var INIT_NEW_ENTRY = "enter new entry...";
 
 
     var todos = [{
         text : "test"
         }, {
-        text : "due"
+        text : "due",
+        todos : [{
+            text : "subtask!!"
+        }]
         }, {
         text : "trete"
         }, {
@@ -25,7 +28,7 @@
         .controller('ListCtrl', ["$scope", function ($scope) {
                 $scope.todos = todos;
                 $scope.newEntry = "";
-                
+
                 $scope.addNew = function () {
                     if ($scope.newEntry && $scope.newEntry != "") {
                         $scope.todos.unshift({
@@ -34,17 +37,56 @@
                         $scope.newEntry = "";
                     }
                 };
+        }])
+        .controller('TodoCtrl', ["$scope", function ($scope) {
+                console.log("todo", $scope.todo);
+
                 $scope.setEdit = function (e, index, edit) {
-                    $scope.todos[index].edit = edit;
+                    console.log("***", $scope.todo);
+                    $scope.todo.edit = edit;
                     if (edit) {
                         var input = document.getElementById("input-" + index);
                         setTimeout(function () {
                             input.focus();
-                            console.log(edit, $scope.todos);
+                            console.log(edit, $scope.todo);
                         }, 1)
                     }
                 };
 
-        }]);
+        }])
+        .directive('todoList', function () {
+            return {
+                restrict : "E",
+                scope : {
+                    todos : "=",
+                    setEdit : "&"
+                },
+                templateUrl : "list/todo-list.html"
+            };
+        })
+        .directive('todo', function ($compile) {
+            return {
+                restrict : "E",
+                templateUrl : "list/todo.html",
+                link : function (scope, element, attrs) {
+                    if (angular.isArray(scope.todo.todos)) {
+                        $compile('<todo-list todos="todo.todos"></todo-list>')(scope, function (cloned, scope) {
+                            element.append(cloned);
+                        });
+                    }
+                }
+            };
+        })
+        .directive('focusMe', function ($timeout) {
+            return {
+                link : function (scope, element, attrs) {
+                    scope.$watch(attrs.focusMe, function (value) {
+                        if (value === true) {
+                            element[0].focus();
+                        }
+                    });
+                }
+            };
+        });
 
 })();
